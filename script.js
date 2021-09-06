539a09f8ce89d9c39cd2385296587dbab233fe53
janniklas-krueger <jan.krueger@elementsystems.de>
2021-09-06 18:38:05 +0200

    script.js mit personen-datenobjekten, events.js mit Aenderungen am DOM

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-alert("Hello World!");
+var Person = function(lastName, firstName, height, hair, eyes)
+{
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.height = height;
+    this.hair = hair;
+    this.eyes = eyes;
+    this.print = function(doc){
+        if(doc)
+        {
+         printName(this.firstName, this.lastName, true);
+         printAppearance(this.height, this.hair, this.eyes, true);
+        }
+        else
+        {
+         printName(this.firstName, this.lastName, false);
+         printAppearance(this.height, this.hair, this.eyes, false);
+        }
+    }
+}
+
+var person = new Person("Mustermann", "Max", 170, "brown", "blue");
+
+
+var person2 =
+{
+    lastName: "Smith",
+    firstName: "John",
+    height: 180,
+    hair: "black",
+    eyes: "grey",
+    print: function(doc){
+        if(doc)
+        {
+            printName(this.firstName, this.lastName, true);
+            printAppearance(this.height, this.hair, this.eyes, true);
+        }
+        else
+        {
+            printName(this.firstName, this.lastName, false);
+            printAppearance(this.height, this.hair, this.eyes, false);
+        }
+    }
+};
+
+
+function printName(fN, lN, doc)
+{
+    if(doc === true)
+    {
+        document.write(fN + " " + lN);
+    }
+    else
+    {
+        console.log(fN + " " + lN);
+    }
+}
+
+function printAppearance(height, hair, eyes, doc)
+{
+    if(doc === true)
+    {
+        document.write(height + " cm, " + hair + " hair, " + eyes + " eyes");
+    }
+    else
+    {
+        console.log(height + " cm, " + hair + " hair, " + eyes + " eyes");
+    }
+}
