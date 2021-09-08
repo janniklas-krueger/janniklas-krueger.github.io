1a952a1c196a0fff7243dcb10df1b66571151a15
janniklas-krueger <jan.krueger@elementsystems.de>
2021-09-08 16:46:45 +0200

    Formular 'Tier zum Petstore hinzufuegen'

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-var Person = function(lastName, firstName, height, hair, eyes)
+function Pet(id, category, name, status)
 {
-    this.lastName = lastName;
-    this.firstName = firstName;
-    this.height = height;
-    this.hair = hair;
-    this.eyes = eyes;
-    this.print = function(doc){
-        if(doc)
-        {
-         printName(this.firstName, this.lastName, true);
-         printAppearance(this.height, this.hair, this.eyes, true);
-        }
-        else
-        {
-         printName(this.firstName, this.lastName, false);
-         printAppearance(this.height, this.hair, this.eyes, false);
-        }
-    }
+    this.id = id;
+    this.category = category;
+    this.name = name;
+    this.status = status;
 }
 
-var person = new Person("Mustermann", "Max", 170, "brown", "blue");
+function Category(id, name)
+{
+    this.id = id;
+    this.name = name;
+}
+
+var form = document.forms.form;
+
+form.onsubmit = function(event)
+{
+    console.log(event);
+
+    const cat = new Category(form.cat_id.value, form.category.value);
+    const pet = new Pet(form.id.value, cat, form.name.value, form.status.value);
+
+    const url = 'https://petstore.swagger.io/v2/pet';
+    fetch(url,
+          {
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify(pet),
+              method: "POST"
+          })
+          .then
+          (
+              function (value)
+              {
+                  alert("Daten gesendet!");
+                  console.log(JSON.stringify(pet));
+                  console.log(value);
+              },
+              function (reason)
+              {
+                  alert("Daten konnten nicht gesendet werden");
+              }
+          );
+    /*.then(stuff => stuff.json())
+    .then((json) => {alert(JSON.stringify(json))})*/
+    event.preventDefault();
+}
 
 
-var person2 =
+
+/*var person2 =
 {
     lastName: "Smith",
     firstName: "John",
@@ -41,10 +67,10 @@ var person2 =
             printAppearance(this.height, this.hair, this.eyes, false);
         }
     }
-};
+};*/
 
 
-function printName(fN, lN, doc)
+/*function printName(fN, lN, doc)
 {
     if(doc === true)
     {
@@ -66,4 +92,4 @@ function printAppearance(height, hair, eyes, doc)
     {
         console.log(height + " cm, " + hair + " hair, " + eyes + " eyes");
     }
-}
+}*/
