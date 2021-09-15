afc03b8885b8899e1bf18e07f96272e52e36fa6d
janniklas-krueger <jan.krueger@elementsystems.de>
2021-09-15 11:19:00 +0200

    Fehler vom falschen Rebase beseitigen

--- events.js
+++ events.js
@@ -1,30 +1,3 @@
-// Hintergrundfarben
-var background = document.getElementsByTagName("body")[0];
-var colours = ["red", "orange", "yellow", "orange"];
-var counter = 0;
-
-var changer = setInterval(changeBackgroundColour, "3000");
-
-function changeBackgroundColour()
-{
-    background.style.background = colours[counter];
-    counter++;
-    if(counter == colours.length)
-    {
-        counter = 0;
-    }
-}
-
-
-///// User-Events /////
-
-// Document
-document.onclick = function()
-{
-    clearInterval(changer);
-    console.log(colours[counter]);
-}
-
 // Überschrift
 var headline = document.getElementsByTagName("h1")[0];
 headline.onclick = function()
